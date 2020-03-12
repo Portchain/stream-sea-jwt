0721a5ae43474f976117f12d71bf8c7f2a574a40
Sergei Patiakin <sergei.patiakin@gmail.com>
2020-03-12 13:53:03 +0100

    Generate asymmetric keypair

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const yargs = require('yargs')
 const jwt = require('jsonwebtoken')
+const keypair = require('keypair')
+const fs = require('fs')
 
 const defaultPayload = {
   "exp": 1893456000, // Expires 2030-01-01
@@ -7,21 +9,34 @@ const defaultPayload = {
 }
 
 yargs.scriptName("stream-sea-jwt")
-  .command('$0', 'Sign a JWT', args => {
-  args.option('jwtSecret', {
-    alias: 's',
-    type: 'string',
-    describe: 'JWT secret'
+  .command('sign', 'Sign a JWT', args => {
+    args.option('jwtSecret', {
+      alias: 's',
+      type: 'string',
+      describe: 'JWT secret'
+    })
+    .option('payload', {
+      alias: 'p',
+      type: 'string',
+      describe: 'payload'
+    })
+    .demandOption('jwtSecret')
+  }, (argv) => {
+    const payload = argv.payload ? JSON.parse(argv.payload) : defaultPayload
+    const jwtSerialized = jwt.sign(payload, argv.jwtSecret)
+    console.log(jwtSerialized)
   })
-  .option('payload', {
-    alias: 'p',
-    type: 'string',
-    describe: 'payload'
+  .command('generate', 'Generate a keypair', args => {
+    args.option('filePrefix', {
+      alias: 'o',
+      type: 'string',
+      describe: 'Prefix for output file names'
+    })
+  }, argv => {
+    console.log('generating...')
+    const {private, public} = keypair()
+    fs.writeSync(fs.openSync(`${argv.filePrefix || ''}private.pem`, 'w'), private)
+    fs.writeSync(fs.openSync(`${argv.filePrefix || ''}public.pem`, 'w'), public)
   })
-  .demandOption('jwtSecret')
-}, (argv) => {
-  const payload = argv.payload ? JSON.parse(argv.payload) : defaultPayload
-  const jwtSerialized = jwt.sign(payload, argv.jwtSecret)
-  console.log(jwtSerialized)
-})
-.argv
+  .strict()
+  .argv
